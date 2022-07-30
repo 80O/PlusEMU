8199f3109e0bd5535505f1be43b12fd45294eec8
kenny577 <kenneth3535@protonmail.com>
2022-07-31 02:32:34 +0300

    Change code style

--- Core/ConsoleCommands.cs
+++ Core/ConsoleCommands.cs
@@ -18,24 +18,24 @@ public static class ConsoleCommands
             {
                 case "stop":
                 case "shutdown":
-                    {
-                        Log.Warn("The server is saving users furniture, rooms, etc. WAIT FOR THE SERVER TO CLOSE, DO NOT EXIT THE PROCESS IN TASK MANAGER!!");
-                        PlusEnvironment.PerformShutDown();
-                        break;
-                    }
+                {
+                    Log.Warn("The server is saving users furniture, rooms, etc. WAIT FOR THE SERVER TO CLOSE, DO NOT EXIT THE PROCESS IN TASK MANAGER!!");
+                    PlusEnvironment.PerformShutDown();
+                    break;
+                }
                 case "alert":
-                    {
-                        var notice = inputData.Substring(6);
-                        PlusEnvironment.GetGame().GetClientManager()
-                            .SendPacket(new BroadcastMessageAlertComposer(PlusEnvironment.GetLanguageManager().TryGetValue("server.console.alert") + "\n\n" + notice));
-                        Log.Info("Alert successfully sent.");
-                        break;
-                    }
+                {
+                    var notice = inputData.Substring(6);
+                    PlusEnvironment.GetGame().GetClientManager()
+                        .SendPacket(new BroadcastMessageAlertComposer(PlusEnvironment.GetLanguageManager().TryGetValue("server.console.alert") + "\n\n" + notice));
+                    Log.Info("Alert successfully sent.");
+                    break;
+                }
                 default:
-                    {
-                        Log.Error(parameters[0].ToLower() + " is an unknown or unsupported command. Type help for more information");
-                        break;
-                    }
+                {
+                    Log.Error(parameters[0].ToLower() + " is an unknown or unsupported command. Type help for more information");
+                    break;
+                }
             }
         }
         catch (Exception e)
