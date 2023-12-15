e8d68f8543a248a6a0bf5b710a898226a77683b0
Poumpilop <nesquiik3@sfr.fr>
2023-12-15 21:30:29 +0100

    issue : Room settings doesn't do anything. #130

--- Resources/SQLs/Original Database.sql
+++ Resources/SQLs/Original Database.sql
@@ -25141,3 +25141,10 @@ ALTER TABLE `rooms`
 ALTER TABLE `users`
     DROP PRIMARY KEY,
     ADD PRIMARY KEY (`id`)
+
+-- 14_UpdateRoomRightsSettings
+
+ALTER TABLE `rooms` 
+    MODIFY COLUMN `mute_settings` int NOT NULL DEFAULT 1 AFTER `group_id`,
+    MODIFY COLUMN `ban_settings` int NOT NULL DEFAULT 1 AFTER `mute_settings`,
+    MODIFY COLUMN `kick_settings` int NOT NULL DEFAULT 1 AFTER `ban_settings`;
